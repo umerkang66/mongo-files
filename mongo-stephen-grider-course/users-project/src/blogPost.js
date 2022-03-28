ed72861fbf30515be055ed5dab4e85a9e4cb1b4a
Umer Kang <umerkang66@gmail.com>
2022-03-28 23:36:23 +0500

    change blogPost.js file

--- mongo-stephen-grider-course/users-project/src/blogPost.js
+++ mongo-stephen-grider-course/users-project/src/blogPost.js
@@ -6,7 +6,12 @@ const blogPostSchema = new mongoose.Schema({
   content: String,
   // Here we have to reference of another document from another collection, in this example blog post document should have reference to the comments that this blog post has
   // We are passing the reference to the another model (document sitting the comment collection)
-  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'comment' }],
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'comment',
+    },
+  ],
 });
 
 // Creating the model out of schema
